7adbaf18dfed6399a332f17aa7cfe777622f1150
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-07 19:20:05 +0530

    start server on different url

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-const mongoose = from("mongoose");
-const express = from("express");
-const http = from("http");
-import { processImport } from "@graphql-tools/import";
-import { resolvers } from "./src/graphql/resolvers";
-const typeDefs = processImport("./src/graphql/schema.graphql");
-
+import cors from "cors";
+import express from "express";
+import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.mjs";
+import http from "http";
+import mongoose from "mongoose";
+import resolvers from "./src/graphql/resolvers.js";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchema } from "@graphql-tools/load";
 const app = express();
 const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
+  typeDefs: await loadSchema("./src/graphql/schema.graphql", {
+    loaders: [new GraphQLFileLoader()],
+  }),
+  resolvers: resolvers, 
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const startApolloServer = async () => {
-  await server.start();
-};
+await server.start();
 
-await startApolloServer();
-
-app.use("/graphql", expressMiddleware(server));
+app.use(
+  "/graphql",
+  cors({
+    origin: ["http://localhost:4000/graphql"],
+  }),
+  express.json(),
+  graphqlUploadExpress(),
+  expressMiddleware(server)
+);
 
 mongoose.connect("mongodb://localhost:27017/movie").then(async () => {
   console.log("database connected");
   await new Promise((resolve) =>
     httpServer.listen({ port: 4000 }, resolve)
-  ).then((res) => console.log(`Server started at: ${res.url}`));
+  ).then((res) => console.log(`Server started at: http://localhost:4000`));
 });

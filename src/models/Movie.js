32797d9ce90bb3573b1259fde23d337441151931
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-07 19:21:42 +0530

    fix: chnage type to module

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema(
   {
@@ -19,4 +19,4 @@ const movieSchema = new mongoose.Schema(
 
 const Movie = mongoose.model("Movie", movieSchema);
 
-module.exports = Movie;
+export default Movie;
